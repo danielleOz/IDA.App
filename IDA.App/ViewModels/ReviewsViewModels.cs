63544570be9807f99fe587787603b6c340a685e4
danielle oz <danielle.oz.do@gmail.com>
2022-02-13 13:24:29 +0200

    fff

---
+++ IDA.App/ViewModels/ReviewsViewModels.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using IDA.App.Models;
+using IDA.App.Services;
+using IDA.App.Views;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace IDA.App.ViewModels
+{
+    class ReviewsViewModels:ViewModelBase
+    {
+
+        #region is worker
+        private bool isWorker;
+        public bool IsWorker
+        {
+            get => this.isWorker;
+            set
+            {
+                if (this.current.User.IsWorker)
+                {
+                    this.isWorker = value;
+                    OnPropertyChanged("IsWorker");
+                }
+
+            }
+        }
+        #endregion
+
+
+        #region isnt worker
+        private bool isntWorker;
+        public bool IsntWorker
+        {
+            get => this.isntWorker;
+            set
+            {
+                if (!this.current.User.IsWorker)
+                {
+                    this.isntWorker = value;
+                    OnPropertyChanged("IsntWorker");
+                }
+
+            }
+        }
+        #endregion
+    }
+}
